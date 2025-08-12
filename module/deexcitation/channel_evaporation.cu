99312ceff02d0e408f7abfcce0ad0fe346d9766e
dlc2048 <dlc2048@postech.ac.kr>
2025-08-12 18:42:39 +0900

    restore emit particle energy sampler

--- module/deexcitation/channel_evaporation.cu
+++ module/deexcitation/channel_evaporation.cu
@@ -499,7 +499,6 @@ namespace deexcitation {
                 float delta1 = fission::pairingCorrection(cache_zaev[threadIdx.x].x, cache_zaev[threadIdx.x].y - cache_zaev[threadIdx.x].x);
 
                 // rejection unity
-                /*
                 float x, w;
                 float wmax = mcutil::cache_univ[CUDA_WARP_SIZE + 4 * blockDim.x + threadIdx.x];
                 do {
@@ -509,8 +508,7 @@ namespace deexcitation {
 
                 // return x + eke_min;
                 return cache_zaev[threadIdx.x].y > 4 ? x + eke_min : eke_max;
-                */
-                return eke_max;
+                // return eke_max;
             }
 
 
